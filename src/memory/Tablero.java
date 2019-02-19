071089162a80d35791dd0bab5529f2fca5607a14
Jose <Jose@192.168.52.36>
2019-02-19 09:54:34 +0100

    Dia 2

--- src/memory/Tablero.java
+++ src/memory/Tablero.java
@@ -5,10 +5,29 @@
  */
 package memory;
 
+import javafx.scene.shape.Rectangle;
+
 /**
  *
  * @author Tomas
  */
 public class Tablero {
+    Tablero tablero = new Tablero();
     
+    Rectangle rectangle = new Rectangle(20,10,400,600);
+    Rectangle rectangle2 = new Rectangle(40,10,400,600);
+    Rectangle rectangle3 = new Rectangle(60,10,400,600);
+    Rectangle rectangle4 = new Rectangle(80,10,400,600);
+    Rectangle rectangle5 = new Rectangle(20,30,400,600);
+    Rectangle rectangle6 = new Rectangle(40,30,400,600);
+    Rectangle rectangle7 = new Rectangle(60,30,400,600);
+    Rectangle rectangle8 = new Rectangle(80,30,400,600);
+    Rectangle rectangle9 = new Rectangle(20,50,400,600);
+    Rectangle rectangle10 = new Rectangle(40,50,400,600);
+    Rectangle rectangle11 = new Rectangle(60,50,400,600);
+    Rectangle rectangle12 = new Rectangle(80,50,400,600);
+    Rectangle rectangle13 = new Rectangle(20,70,400,600);
+    Rectangle rectangle14 = new Rectangle(40,70,400,600);
+    Rectangle rectangle15 = new Rectangle(60,70,400,600);
+    Rectangle rectangle16 = new Rectangle(80,70,400,600);
 }
